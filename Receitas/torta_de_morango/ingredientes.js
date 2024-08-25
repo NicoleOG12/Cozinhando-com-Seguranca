11bdea25a3d446fda49daed7fc8c610a10394769
nicole.goncalves6@etec.sp.gov.br <nicole.goncalves6@etec.sp.gov.br>
2024-08-24 21:32:46 -0300

    Torta de Morango pronta

---
+++ Receitas/torta_de_morango/ingredientes.js
@@ -0,0 +1,22 @@
+function revealImage() {
+    var element = document.getElementById('farinha');
+    if (element) element.classList.add('reveal');
+    
+    element = document.getElementById('manteiga');
+    if (element) element.classList.add('reveal');
+    
+    element = document.getElementById('acucar');
+    if (element) element.classList.add('reveal');
+    
+    element = document.getElementById('leite_condensado');
+    if (element) element.classList.add('reveal');
+    
+    element = document.getElementById('morango');
+    if (element) element.classList.add('reveal');
+    
+    element = document.getElementById('essencia_baunilha');
+    if (element) element.classList.add('reveal');
+    
+    element = document.getElementById('geleia_morango');
+    if (element) element.classList.add('reveal');
+}
