be1ad84e065ce67b1ed9678cfb08fb14d2d21b44
nicole.goncalves6@etec.sp.gov.br <nicole.goncalves6@etec.sp.gov.br>
2024-08-26 15:50:09 -0300

    Correção menu

--- Carrosel de receitas.js
+++ Carrosel de receitas.js
@@ -1,26 +1,12 @@
 let currentSlide = 0;
-const slides = document.querySelectorAll('.carrossel-item');
-const imagemSlide = document.getElementById('imagem-slide');
-
-// Array com os caminhos das imagens correspondentes a cada slide
-const imagens = [
-    './imagens/farinha.png',
-    './imagens/nicoleChibiFundoRosa.png',
-];
+const slides = document.querySelectorAll('.carrossel-item'); 
 
 function showSlide(index) {
     slides[currentSlide].classList.remove('ativo'); 
-    imagemSlide.classList.remove('ativo');  // Inicia o fade out da imagem atual
-
     currentSlide = (index + slides.length) % slides.length;
-    slides[currentSlide].classList.add('ativo');
-
-    // Trocar a imagem da receita após o fade out
-    setTimeout(() => {
-        imagemSlide.src = imagens[currentSlide];
-        imagemSlide.classList.add('ativo');  // Inicia o fade in da nova imagem
-    }, 500);  // Ajuste o tempo para coincidir com a duração da transição (0.5s)
+    slides[currentSlide].classList.add('ativo'); 
 }
+
 
 function nextSlide() {
     showSlide(currentSlide + 1);
@@ -31,11 +17,13 @@ function prevSlide() {
 }
 
 document.addEventListener('keydown', function(event){
-    if(event.key === 'ArrowRight') {
-        nextSlide();
-    } else if (event.key === 'ArrowLeft') {
-        prevSlide();
-    } 
-});
+        if(event.key === 'ArrowRight') {
+            nextSlide();
+           
+        } else if (event.key === 'ArrowLeft') {
+            prevSlide();
+        } 
+    
+    });
 
-showSlide(currentSlide);  // Mostrar o slide inicial
+showSlide(currentSlide);
