f28de11eebf45f47a2a709d00989b9d98bd2d408
claraaraujo2 <144171290+claraaraujo2@users.noreply.github.com>
2024-08-27 16:02:16 -0300

    Delete Receitas directory

--- Receitas/scripts.js
+++
@@ -1,8 +0,0 @@
-function disabled(card) {
-    document.querySelectorAll('.opcao').forEach(opcao => {
-        if (opcao !== card) {
-            opcao.classList.add('disabled');
-        }
-    });
-}
-    
