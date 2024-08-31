c2a5a67f6c387b4b11b2fe21d4ee924aabc4771b
julia <julia.silva1490@etec.sp.gov.br>
2024-08-31 16:02:07 -0300

    adicionando perguntas

---
+++ Receitas/red_velvet/ingredientes.js
@@ -0,0 +1,22 @@
+function revealImage() {
+    var element = document.getElementById('ovo');
+    if (element) element.classList.add('reveal');
+    
+    element = document.getElementById('acucar');
+    if (element) element.classList.add('reveal');
+    
+    element = document.getElementById('farinha');
+    if (element) element.classList.add('reveal');
+    
+    element = document.getElementById('chocolate');
+    if (element) element.classList.add('reveal');
+    
+    element = document.getElementById('oleo');
+    if (element) element.classList.add('reveal');
+    
+    element = document.getElementById('leite');
+    if (element) element.classList.add('reveal');
+    
+    element = document.getElementById('fermento');
+    if (element) element.classList.add('reveal');
+}
